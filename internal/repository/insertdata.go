1310405ff0996dd1621206b5ecd36b8f099f53b5
Terry Hsiesh <lin020488995@gmail.com>
2022-07-02 23:33:27 +0800

    db test

--- internal/repository/insertdata.go
+++ internal/repository/insertdata.go
@@ -20,31 +20,3 @@ func QueryCampByCampName(campName string) (model.Camp, error) {
 	return camp, err
 
 }
-func InsertTest() {
-	// if _, err := db.Exec("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)"); err != nil {
-
-	// 	fmt.Sprintf("Error creating database table: %q", err)
-	// 	return
-	// }
-
-	// if _, err := db.Exec("INSERT INTO ticks VALUES (now())"); err != nil {
-	// 	fmt.Sprintf("Error incrementing tick: %q", err)
-	// 	return
-	// }
-
-	// rows, err := db.Query("SELECT tick FROM ticks")
-	// if err != nil {
-	// 	fmt.Sprintf("Error reading ticks: %q", err)
-	// 	return
-	// }
-
-	// defer rows.Close()
-	// for rows.Next() {
-	// 	var tick time.Time
-	// 	if err := rows.Scan(&tick); err != nil {
-	// 		fmt.Sprintf("Error scanning ticks: %q", err)
-	// 		return
-	// 	}
-	// }
-
-}
